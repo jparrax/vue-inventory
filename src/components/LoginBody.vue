2f2da050a8796a052a9f1fe5cd07b986d7053f6b
ppuarat <boomer_saw@hotmail.com>
2020-04-01 13:14:41 +1300

    Added new features

--- src/components/LoginBody.vue
+++ src/components/LoginBody.vue
@@ -16,7 +16,7 @@
                                 Username
                             </b-col>
                             <b-col cols="8">
-                                <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
+                                <b-form-input v-model="username" placeholder="Enter your name"></b-form-input>
                             </b-col>
                         </b-row>
                         <br>
@@ -25,7 +25,7 @@
                                 Password
                             </b-col>
                             <b-col  cols="8">
-                                <b-form-input type="password" v-model="text" placeholder="Enter your password"></b-form-input>
+                                <b-form-input type="password" v-model="password" placeholder="Enter your password"></b-form-input>
                             </b-col>
                         </b-row>
                         <br>
@@ -46,12 +46,18 @@ import { bus } from '../main.js'
 export default {
   data () {
     return {
-        text: ""
+        username: "",
+        password: ""
     }
   },
   methods: {
     logIn: function () {
-      bus.$emit('logIn')
+        if(this.username == "admin" && this.password == "admin"){
+
+            bus.$emit('logIn')
+        }else{
+            alert("Username or Password not found!");
+        }
     }
   }
 }
